645380f37b088138fe1c397854dd3ef867ca0761
Forlan Ordoñez <nalrofor@gmail.com>
2025-06-27 15:10:08 -0500

    feat: agregue metodos

--- semana 2/tipos de datos/typedatos.js
+++ semana 2/tipos de datos/typedatos.js
@@ -3,5 +3,15 @@ let num = 123.456;
 const num2 = 10;
 console.log(num.toFixed(2));
 
-let greeting = 'hello, world';
-console.log(greeting.toUpperCase());
+//metodo string 
+
+//toUpperCase() //convierte todas las letras de la cadena mayuscula
+//toLowerCase() //convierte todas las letras de la cadena a minuscula
+//CharacterData(n) //devuelve el caracter en la posicion n
+//includes(cadena)  //verifica si la cadena contiene un texto especifico y deculeve true o false
+//substring(Inicio, fin) //devuelve una subcadena desde la posicion inicio hasta fin
+//replace(viejo, nuevo)  //reempalaza uns prte de la cadena por otra
+
+
+let greeting = 'Hello, world';
+console.log(greeting.toLocaleLowerCase());
